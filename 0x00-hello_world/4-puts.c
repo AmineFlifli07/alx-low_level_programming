3a69cc39da4c5f7ee3b6798834fcf69479724cae
amine <amine@mail.com>
2023-09-07 23:21:04 +0000

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * 
- * Description: A c program that print some words
- *
- * Return : Always 0 (Success)
-*/
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-
 	return (0);
 }
