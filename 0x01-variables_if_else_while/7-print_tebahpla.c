7a02d688e2df3a811e6ef63eebd40f3f56d4bbdf
amine <amine@mail.com>
2023-09-08 20:44:25 +0000

    tp7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
