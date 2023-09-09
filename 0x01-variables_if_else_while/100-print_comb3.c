e62714a9ffcf9223abe3a7c30b0e7eb297a050bc
amine <amine@mail.com>
2023-09-09 02:58:27 +0000

    tp10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int tens;
+	int ones;
+
+	for (tens = '0'; tens <= '9'; tens++) /*print tens place*/
+	{
+		for (ones = '0'; ones <= '9'; ones++) /*print ones place*/
+		{
+			putchar(tens);
+			putchar(ones);
+			if (!(tens == '9' && ones == '9')) /*skip comma at end*/
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
