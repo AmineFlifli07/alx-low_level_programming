7f3f0b7293e4a4f34e5c51145de69a4820ffabfa
amine <amine@mail.com>
2023-09-12 23:52:05 +0000

    tp1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
