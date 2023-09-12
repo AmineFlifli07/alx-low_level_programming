322ee16c78131832cbd967c1bad841751cd88873
amine <amine@mail.com>
2023-09-12 23:59:37 +0000

    tp1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,9 @@
 #include "main.h"
 
 /**
- * main - entry point
  *
- * Description: false or true validation
+ * print_alphabet - utilezes on the _putchar to print the alphabet
  *
- *print_alphabet - utilezes on the _putchar to print the alphabet
- *
- * Return: 0 (success)
  */
 
 void print_alphabet(void)
