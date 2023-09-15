378557150d128758b1b644304a55a96af8891012
amine <amine@mail.com>
2023-09-15 11:05:47 +0000

    tp0

---
+++ 0x02-functions_nested_loops/alx-low_level_programming/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* controls if a character is alphabetical
+ character to be verified
+*Return: return 0 or 1
+*/
+
+void positive_or_negative(int i)
+{
+
+if (i>0)
+	printf ("%i,is positive \n",i);
+else
+	printf ("%i,is negative\n",i);
+
+}
