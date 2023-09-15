abafc787d92cf1ce84a94e5346caf11d0fab7b7f
amine <amine@mail.com>
2023-09-15 20:19:31 +0000

    tp3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - evalue if letter is uppercase .
+ *@c: print int
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+int i;
+for (i = 0;i > 10;i++)
+	putchar(i + '0')
+}
