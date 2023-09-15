34080fde7b3fd3b0bfee00799c2fc4670e4bfe58
amine <amine@mail.com>
2023-09-15 20:57:30 +0000

    tp1

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,36 @@
 
 void more_numbers(void)
 {
-	int a, b;
+int i;
+int first_number;
+int second_number;
+int result;
 
-	for (a = 0;a < 10;a++)
-	{
-		for (b = 0;b < 14;b++)
-		{	
-			putchar('0' + b);
-		}
+i = 0;
+result = 0;
+while (i < 10)
+{
+while (result <= 14)
+{
+if (result < 10)
+{
+second_number = result;
+}
 
+else
+{
+first_number = result / 10;
+second_number = result % 10;
+_putchar (first_number + '0');
+}
 
-	}
+_putchar (second_number + '0');
+
+result++;
+}
+i++;
+result = 0;
+_putchar ('\n');
+}
+
 }
