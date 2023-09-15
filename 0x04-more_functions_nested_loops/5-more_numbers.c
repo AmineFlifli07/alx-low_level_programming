0be6d63834b02744a4b1bad053fa15235cf5d1ca
amine <amine@mail.com>
2023-09-15 20:58:00 +0000

    tp1

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -30,13 +30,13 @@ second_number = result % 10;
 _putchar (first_number + '0');
 }
 
-_putchar (second_number + '0');
+putchar (second_number + '0');
 
 result++;
 }
 i++;
 result = 0;
-_putchar ('\n');
+putchar ('\n');
 }
 
 }
