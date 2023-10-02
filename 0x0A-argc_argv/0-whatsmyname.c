9feb31bddfca34ea67caae817618b4fd4ff722b7
amine <amine@mail.com>
2023-10-02 08:43:44 +0000

    tp0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ * @argc : a parameter
+ * @argv : b parameter
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
