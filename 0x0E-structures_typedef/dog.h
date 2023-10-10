5efff6c05f21a37c0403d5493b41c83dda5cee65
amine <amine@pop-os.localdomain>
2023-10-10 18:45:11 +0100

    tp0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,15 @@
-struct dog {char *name;
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Struct dog
+ * @name: name
+ * @age: age
+ * @owner:  owner
+ * Description: This struct is for dog
+ */
+struct dog
+{
+	char *name;
 	float age;
-	char *owner;};
+	char *owner;
+};
